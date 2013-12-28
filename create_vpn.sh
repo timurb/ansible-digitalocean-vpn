574ffc0c7ab5b66fa3cc54cf4e5e336a14414061
Timur Batyrshin <erthad@gmail.com>
2013-12-28 18:34:26 +0400

    split single playbook run into 3 separate runs, wait for the box to get up, produce a message when box is up

--- create_vpn.sh
+++ create_vpn.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-time ansible-playbook -K vpn_digital_ocean.yml $@
+ansible-playbook -K vpn_digital_ocean.yml $@
+
+echo -n "Waiting for host to get up "
+while ! ping -c 1 -W 5 vpn > /dev/null; do
+  echo -n "."
+  sleep 1 
+done
+echo
+
+echo -n "Waiting for SSH server on the host to get up "
+while ! netcat -z vpn 22; do
+  echo -n "."
+  sleep 1 
+done
+echo
+
+ansible-playbook vpn.yml $@
+ansible-playbook vpn_teardown.yml $@
+
+which notify-send > /dev/null && notify-send 'VPN gateway is online'
